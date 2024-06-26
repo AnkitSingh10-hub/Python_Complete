c02be7ca71d6eaf6649254921b0c06c277d1928e
Ankit Singh <ankitsinghtheweeknd@gmail.com>
2024-06-26 12:35:30 +0545

    nbb

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,7 @@
 # A dictionary is a key value pair which is ordered(indexed), changeable(mutable) and do not allow duplicates.
 thisdict = {"brand": "Ford", "model": "Mustang", "Year": 1964}
 print(thisdict)
-
+print(thisdict.values())
 for i in thisdict.values():
     print(i)
 
