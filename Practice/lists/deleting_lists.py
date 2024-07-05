519b54f5a9ab658ed016beffe439b79b271ee2c9
Ankit Singh <ankitsinghtheweeknd@gmail.com>
2024-07-05 14:47:50 +0545

    mno

--- Practice/lists/deleting_lists.py
+++ Practice/lists/deleting_lists.py
@@ -1,13 +1,13 @@
+list_3 = [10, 20, 30, 40, 50]
+print(list_3)
+list_3.pop()
+print(list_3)
 
 # deleting lists
 list_2 = [10, 20, 30, 40, 50]
 list_2.remove(30)
 print(list_2)
 
-list_3 = [10, 20, 30, 40, 50]
-print(list_3)
-list_3.pop()
-print(list_3)
 
 list_4 = [10, 20, 30, 40, 50]
 list_4.clear()
