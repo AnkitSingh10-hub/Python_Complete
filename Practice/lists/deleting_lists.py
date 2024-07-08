5f439e63b8a1228493f2923d279b04bd4c72c342
Ankit Singh <ankitsinghtheweeknd@gmail.com>
2024-07-08 11:54:58 +0545

    x

--- Practice/lists/deleting_lists.py
+++ Practice/lists/deleting_lists.py
@@ -1,9 +1,10 @@
+#deleting list data by its index
 list_3 = [10, 20, 30, 40, 50]
 print(list_3)
 list_3.pop()
 print(list_3)
 
-# deleting lists
+# deleting list data specifically
 list_2 = [10, 20, 30, 40, 50]
 list_2.remove(30)
 print(list_2)
